4ce5b8cc713147a8a9065acc60f1287a3f011646
michael <mschiumo18@gmail.com>
2023-12-03 12:42:22 -0500

    add quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,1 +1,21 @@
-export default function Quiz() {}
+import { useState } from "react";
+import QUESTIONS from '../questions.js';
+
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+
+    return (
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className="answer">
+
+                    <button>         {answer}</button>
+                </li>)}
+            </ul>
+        </div>
+    )
+
+}
