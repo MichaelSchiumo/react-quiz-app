2877f10b05b5381e155fb94ce34a2ca3c6bbccfa
michael <mschiumo18@gmail.com>
2023-12-04 14:30:35 -0500

    app complete

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,22 +2,31 @@ import logo from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 export default function Summary({ userAnswers }) {
+    const skippedAnswers = userAnswers.filter(answer => answer === null);
+    const correctAnswers = userAnswers.filter((answer, index) => answer === QUESTIONS[index].answers[0]);
+
+    const skippedAnswersShare = Math.round((skippedAnswers.length / userAnswers.length) * 100);
+
+    const correctAnswersShare = Math.round((correctAnswers.length / userAnswers.length) * 100);
+
+    const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
     return (
         <div id="summary">
             <img src={logo} alt='quiz complete' />
             <h2>Quiz Completed!</h2>
             <div id="summary-stats">
                 <p>
-                    <span className="number">1%</span>
+                    <span className="number">{skippedAnswersShare}%</span>
                     <span className="skipped">skipped</span>
                 </p>
                 <p>
-                    <span className="number">10%</span>
-                    <span className="skipped">skipped</span>
+                    <span className="number">{correctAnswersShare}%</span>
+                    <span className="skipped">correct</span>
                 </p>
                 <p>
-                    <span className="number">20%</span>
-                    <span className="skipped">skipped</span>
+                    <span className="number">{wrongAnswersShare}%</span>
+                    <span className="skipped">wrong</span>
                 </p>
             </div>
             <ol>
